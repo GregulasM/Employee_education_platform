d2d87c69efbaf25d010f330e268efdabcf5608a2
GregulasM <miha125rus@yandex.ru>
2025-05-02 04:46:07 +1000

    Подредактирована страница новостей

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -8,52 +8,48 @@
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="#888888" d="M17.707 8.293a.999.999 0 1 0-1.414 1.414L17.586 11H13V6.414l1.293 1.293a.997.997 0 0 0 1.414 0a1 1 0 0 0 0-1.414L12 2.586L8.293 6.293a.999.999 0 1 0 1.414 1.414L11 6.414V11H6.414l1.293-1.293a.999.999 0 1 0-1.414-1.414L2.586 12l3.707 3.707a.997.997 0 0 0 1.414 0a1 1 0 0 0 0-1.414L6.414 13H11v4.586l-1.293-1.293a.999.999 0 1 0-1.414 1.414L12 21.414l3.707-3.707a.999.999 0 1 0-1.414-1.414L13 17.586V13h4.586l-1.293 1.293a.999.999 0 1 0 1.414 1.414L21.414 12z"/></svg>
         </button>
       </div>
+      <ul class="flex-col p-2 font-semibold list rounded-box shadow-md text-shadow-lg text-black ">
 
-      <div class="flex-col p-2 font-semibold text-shadow-lg rounded-box shadow-md text-black">
-        <div>
-          <ul class="flex-col p-2 font-semibold list rounded-box shadow-md text-black">
+        <li class="pb-2 text-xs self-center opacity-60 tracking-wide">
+          <div class="join">
+            <button v-on:click="prevPage" :disabled="page === 1"
+                    class="join-item btn btn-sm bg-red-500/50 btn-square btn-ghost hover:bg-red-500/50 hover:border-none border-none">«</button>
 
-            <li class="pb-2 text-xs self-center opacity-60 tracking-wide">
-              <div class="join">
-                <button v-on:click="prevPage" :disabled="page === 1"
-                        class="join-item btn btn-sm bg-red-500/50 btn-square btn-ghost hover:bg-red-500/50 hover:border-none border-none">«</button>
+            <button v-for="n in totalPages" :key="n"
+                    v-on:click="page = n"
+                    class="join-item btn btn-sm btn-square btn-ghost hover:bg-red-500/50 hover:border-none border-none" >{{ n }}</button>
 
-                <button v-for="n in totalPages" :key="n"
-                        v-on:click="page = n"
-                        class="join-item btn btn-sm btn-square btn-ghost hover:bg-red-500/50 hover:border-none border-none" >{{ n }}</button>
+            <button v-on:click="nextPage" :disabled="page === totalPages"
+                    class="join-item btn btn-sm bg-red-500/50 btn-square btn-ghost hover:bg-red-500/50 hover:border-none border-none">»</button>
+          </div>
+        </li>
 
-                <button v-on:click="nextPage" :disabled="page === totalPages"
-                        class="join-item btn btn-sm bg-red-500/50 btn-square btn-ghost hover:bg-red-500/50 hover:border-none border-none">»</button>
-              </div>
-            </li>
+        <li v-for="item in pagedNews" :key="item.slug" class="list-row shadow-md shadow-orange-200 mb-2">
+          <div>
+            <img class="size-10 rounded-box" :src="item.image" alt="preview"/>
+          </div>
+          <div>
+            <div>{{ item.title }}</div>
+            <div class="text-xs uppercase font-semibold opacity-60">{{ item.type }}</div>
+          </div>
+          <p class="list-col-wrap text-xs">
+            {{ item.excerpt }}
+          </p>
 
-            <li v-for="item in pagedNews" :key="item.slug" class="list-row shadow-md shadow-orange-200">
-              <div>
-                <img class="size-10 rounded-box" :src="item.image" alt="preview"/>
-              </div>
-              <div>
-                <div>{{ item.title }}</div>
-                <div class="text-xs uppercase font-semibold opacity-60">{{ item.type }}</div>
-              </div>
-              <p class="list-col-wrap text-xs">
-                {{ item.excerpt }}
-              </p>
+          <HoverCard>
+            <HoverCardTrigger>
+              <button v-on:click="goToNews(item.slug)" class="btn btn-square btn-ghost hover:bg-red-500/50 hover:border-none border-none">
+                <svg class="size-[2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#888888" d="M12 21.5c-1.35-.85-3.8-1.5-5.5-1.5c-1.65 0-3.35.3-4.75 1.05c-.1.05-.15.05-.25.05c-.25 0-.5-.25-.5-.5V6c.6-.45 1.25-.75 2-1c1.11-.35 2.33-.5 3.5-.5c1.95 0 4.05.4 5.5 1.5c1.45-1.1 3.55-1.5 5.5-1.5c1.17 0 2.39.15 3.5.5c.75.25 1.4.55 2 1v14.6c0 .25-.25.5-.5.5c-.1 0-.15 0-.25-.05c-1.4-.75-3.1-1.05-4.75-1.05c-1.7 0-4.15.65-5.5 1.5m-1-14c-1.36-.6-3.16-1-4.5-1c-1.2 0-2.4.15-3.5.5v11.5c1.1-.35 2.3-.5 3.5-.5c1.34 0 3.14.4 4.5 1zM13 19c1.36-.6 3.16-1 4.5-1c1.2 0 2.4.15 3.5.5V7c-1.1-.35-2.3-.5-3.5-.5c-1.34 0-3.14.4-4.5 1zm1-2.65c.96-.35 2.12-.52 3.5-.52c1.04 0 1.88.08 2.5.24v-1.5a13.9 13.9 0 0 0-6 .19zm0-2.66c.96-.35 2.12-.53 3.5-.53c1.04 0 1.88.08 2.5.24v-1.5c-.87-.16-1.71-.23-2.5-.23c-1.28 0-2.45.15-3.5.45zM14 11c.96-.33 2.12-.5 3.5-.5c.91 0 1.76.09 2.5.28V9.23c-.87-.15-1.71-.23-2.5-.23c-1.32 0-2.5.15-3.5.46z"/></svg>
+              </button>
+            </HoverCardTrigger>
+            <HoverCardContent class="text-sm opacity-90">
+              Откроется страница с новостью
+            </HoverCardContent>
+          </HoverCard>
+        </li>
 
-              <HoverCard>
-                <HoverCardTrigger>
-                  <button v-on:click="goToNews(item.slug)" class="btn btn-square btn-ghost hover:bg-red-500/50 hover:border-none border-none">
-                    <svg class="size-[2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#888888" d="M12 21.5c-1.35-.85-3.8-1.5-5.5-1.5c-1.65 0-3.35.3-4.75 1.05c-.1.05-.15.05-.25.05c-.25 0-.5-.25-.5-.5V6c.6-.45 1.25-.75 2-1c1.11-.35 2.33-.5 3.5-.5c1.95 0 4.05.4 5.5 1.5c1.45-1.1 3.55-1.5 5.5-1.5c1.17 0 2.39.15 3.5.5c.75.25 1.4.55 2 1v14.6c0 .25-.25.5-.5.5c-.1 0-.15 0-.25-.05c-1.4-.75-3.1-1.05-4.75-1.05c-1.7 0-4.15.65-5.5 1.5m-1-14c-1.36-.6-3.16-1-4.5-1c-1.2 0-2.4.15-3.5.5v11.5c1.1-.35 2.3-.5 3.5-.5c1.34 0 3.14.4 4.5 1zM13 19c1.36-.6 3.16-1 4.5-1c1.2 0 2.4.15 3.5.5V7c-1.1-.35-2.3-.5-3.5-.5c-1.34 0-3.14.4-4.5 1zm1-2.65c.96-.35 2.12-.52 3.5-.52c1.04 0 1.88.08 2.5.24v-1.5a13.9 13.9 0 0 0-6 .19zm0-2.66c.96-.35 2.12-.53 3.5-.53c1.04 0 1.88.08 2.5.24v-1.5c-.87-.16-1.71-.23-2.5-.23c-1.28 0-2.45.15-3.5.45zM14 11c.96-.33 2.12-.5 3.5-.5c.91 0 1.76.09 2.5.28V9.23c-.87-.15-1.71-.23-2.5-.23c-1.32 0-2.5.15-3.5.46z"/></svg>
-                  </button>
-                </HoverCardTrigger>
-                <HoverCardContent class="text-sm opacity-90">
-                  Откроется страница с новостью
-                </HoverCardContent>
-              </HoverCard>
-            </li>
+      </ul>
 
-          </ul>
-        </div>
-      </div>
 
       <div class="mt-8"/>
     </div>
