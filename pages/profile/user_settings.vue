ee47816d63f2ecdbf288f80eb5e88c7812f6e3e6
GregulasM <miha125rus@yandex.ru>
2025-05-07 09:39:14 +1000

    подготовка к настройкам пользователя

--- pages/profile/user_settings.vue
+++ pages/profile/user_settings.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    <h1>Лол</h1>
+  <div class="shadow-md shadow-orange-200 mt-8 ml-8 mr-8 rounded-lg bg-orange-50 opacity-90 h-min">
+
+    <div class="text-xl text-white font-bold text-shadow-lg/20 bg-red-500/50 gap-4 p-2 rounded-t-lg flex justify-between items-center mb-4">
+      <h2>Настройки</h2>
+    </div>
+    <div class="p-2 font-semibold text-shadow-lg p-4 mb-4">
+
+    </div>
+
+
+
+    <div class="mt-8"/>
   </div>
 </template>
