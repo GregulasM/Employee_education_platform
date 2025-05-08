1908188bd2f617d1e56cd02bfb258dd136e6b687
GregulasM <miha125rus@yandex.ru>
2025-05-09 03:26:08 +1000

    Стилизация достижений - аватар, текст, рамки и контейнер

--- pages/profile/achievements.vue
+++ pages/profile/achievements.vue
@@ -13,9 +13,19 @@
                 <h2>{{item.title}}</h2>
               </div>
               <div class="flex-col font-semibold text-shadow-lg">
-                <ScrollArea class="h-72 w-full rounded-md">
-                  <div class="flex-col mr-4 font-semibold text-shadow-lg whitespace-pre-line ml-4">
-                    {{item.content}}
+                <ScrollArea class="h-72 w-full rounded-md mb-2">
+                  <div v-for="item_ach in achItems" :key="item_ach.value" :value="item_ach.value" class="flex-col mr-4 font-semibold text-shadow-lg whitespace-pre-line ml-4">
+                    <div class="flex gap-4 p-2 inset-shadow-sm inset-shadow-red-500/50 rounded-lg mb-2 ">
+                      <div class="avatar self-center ml-2">
+                        <div class="w-12 h-12 rounded shadow-sm shadow-black">
+                          <img :src="item_ach.icon" />
+                        </div>
+                      </div>
+                      <div class="flex-col">
+                        <p class="font-semibold text-sm">{{item_ach.title}}</p>
+                        <p class="font-normal text-xs">{{item_ach.text}}</p>
+                      </div>
+                    </div>
                   </div>
                 </ScrollArea>
               </div>
@@ -40,7 +50,7 @@ import { useRouter } from 'vue-router'
 
 // Буду заменять на fetch с API
 const achItemsLinks= [
-  { value: 'achL-1', title: 'Мастер JS', content: 'Достижение 1 \n Достижение 2\nДостижение 3 \nДостижение 4 \nДостижение 5 \nДостижение 6\nДостижение 5 \nДостижение 6' },
+  { value: 'achL-1', icon: "https://img.daisyui.com/images/profile/demo/batperson@192.webp", title: 'Мастер JS', content: 'Достижение 1 \n Достижение 2\nДостижение 3 \nДостижение 4 \nДостижение 5 \nДостижение 6\nДостижение 5 \nДостижение 6' },
   { value: 'achL-2', title: 'Мастер ASP .NET Core', content: 'Достижение 1 \n Достижение 2\nДостижение 3 \nДостижение 4 \nДостижение 5 \nДостижение 6' },
   { value: 'achL-3', title: 'Мастер C#', content: 'Достижение 1 \n Достижение 2\nДостижение 3 \nДостижение 4 \nДостижение 5 \nДостижение 6' },
   { value: 'achL-4', title: 'Мастер JS', content: 'Достижение 1 \n Достижение 2\nДостижение 3 \nДостижение 4 \nДостижение 5 \nДостижение 6' },
@@ -55,6 +65,17 @@ const achItemsLinks= [
   { value: 'achL-13', title: 'Мастер JS', content: 'Достижение 1 \n Достижение 2\nДостижение 3 \nДостижение 4 \nДостижение 5 \nДостижение 6' },
   { value: 'achL-14', title: 'Мастер ASP .NET Core', content: 'Достижение 1 \n Достижение 2\nДостижение 3 \nДостижение 4 \nДостижение 5 \nДостижение 6' },
   { value: 'achL-15', title: 'Мастер C#', content: 'Достижение 1 \n Достижение 2\nДостижение 3 \nДостижение 4 \nДостижение 5 \nДостижение 6' },
+]
+
+const achItems= [
+  { value: 'achL-1', icon: "https://img.daisyui.com/images/profile/demo/batperson@192.webp", title: 'Мастер JS', text: 'Достижение 1 \n ого текст' },
+  { value: 'achL-2', icon: "https://img.daisyui.com/images/profile/demo/batperson@192.webp", title: 'Мастер JS', text: 'Достижение 1 \n ого текст' },
+  { value: 'achL-3', icon: "https://img.daisyui.com/images/profile/demo/batperson@192.webp", title: 'Мастер JS', text: 'Достижение 1 \n ого текст' },
+  { value: 'achL-4', icon: "https://img.daisyui.com/images/profile/demo/batperson@192.webp", title: 'Мастер JS', text: 'Достижение 1 \n ого текст' },
+  { value: 'achL-5', icon: "https://img.daisyui.com/images/profile/demo/batperson@192.webp", title: 'Мастер JS', text: 'Достижение 1 \n ого текст' },
+  { value: 'achL-6', icon: "https://img.daisyui.com/images/profile/demo/batperson@192.webp", title: 'Мастер JS', text: 'Достижение 1 \n ого текст' },
+  { value: 'achL-7', icon: "https://img.daisyui.com/images/profile/demo/batperson@192.webp", title: 'Мастер JS', text: 'Достижение 1 \n ого текст' },
+  { value: 'achL-8', title: 'Мастер ASP .NET Core', content: 'Достижение 1 \n Достижение 2\nДостижение 3 \nДостижение 4 \nДостижение 5 \nДостижение 6' },
 ]
 
 </script>
