f6d3189902347ea34b4033852b6380becf38797a
GregulasM <miha125rus@yandex.ru>
2025-06-02 10:07:15 +1000

    Изменение структуры проектов, новый дизайн кнопок и оформления

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
+  <div class="bg-gray-700 h-screen">
     <div>
-      <div v-if="!site_store.active_page">
+      <div v-if="!site_store.active_page" class="flex justify-center p-4 gap-4">
         <NuxtLink to="/game_module" v-on:click="site_store.switch_vision()"
-                  class="btn btn-block text-white bg-red-500/50 hover:bg-red-500/70 hover:border-none border-none font-semibold text-shadow-lg/20 shadow-sm shadow-neutral-500 mb-4">
+                  class="btn btn-ghost w-1/4 h-12 text-white bg-cyan-500/90 hover:bg-cyan-700/90 hover:border-none border-none font-semibold text-shadow-lg/20 shadow-sm shadow-black/10 mb-4">
           Модуль геймификации</NuxtLink>
         <NuxtLink to="/course_module" v-on:click="site_store.switch_vision()"
-                  class="btn btn-block text-white bg-red-500/50 hover:bg-red-500/70 hover:border-none border-none font-semibold text-shadow-lg/20 shadow-sm shadow-neutral-500 mb-4">
+                  class="btn btn-ghost w-1/4 h-12 text-white bg-cyan-500/90 hover:bg-cyan-700/90 hover:border-none border-none font-semibold text-shadow-lg/20 shadow-sm shadow-black/10 mb-4">
           Модуль статей</NuxtLink>
         <NuxtLink to="/user_module" v-on:click="site_store.switch_vision()"
-                  class="btn btn-block text-white bg-red-500/50 hover:bg-red-500/70 hover:border-none border-none font-semibold text-shadow-lg/20 shadow-sm shadow-neutral-500 mb-4">
+                  class="btn btn-ghost w-1/4 h-12 text-white bg-cyan-500/90 hover:bg-cyan-700/90 hover:border-none border-none font-semibold text-shadow-lg/20 shadow-sm shadow-black/10 mb-4">
           Модуль пользователей</NuxtLink>
 <!--        router.push('/game_module')-->
       </div>
---
+++ pages/user_module/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div>
+      <div class="flex justify-center p-4 gap-4">
+        <button
+            class="btn btn-ghost w-1/4 h-12 text-white bg-cyan-500/90 hover:bg-cyan-700/90 hover:border-none border-none font-semibold text-shadow-lg/20 shadow-sm shadow-black/10 mb-4"
+            v-on:click="">
+          Сортировка/Фильтрация</button>
+        <button
+            class="btn btn-ghost w-1/4 h-12 text-white bg-amber-500/90 hover:bg-amber-700/90 hover:border-none border-none font-semibold text-shadow-lg/20 shadow-sm shadow-black/10 mb-4"
+            v-on:click="">
+          Обновить</button>
+        <NuxtLink to="/"
+                  class="btn btn-ghost w-1/4 h-12 text-white bg-red-500/90 hover:bg-red-700/90 hover:border-none border-none font-semibold text-shadow-lg/20 shadow-sm shadow-black/10 mb-4"
+                  v-on:click="site_store.switch_vision()">
+          Назад</NuxtLink>
+      </div>
+      <div class="pl-10 pr-10 self-center items-center flex justify-center ">
+        <ScrollArea class="h-100 w-full rounded-md ">
+          <table class="table w-full shadow rounded-lg ">
+            <thead class="sticky top-0 bg-gray-800/90 text-white/80">
+            <tr>
+              <th>ID</th><th>*Колонка_название*</th><th>*Колонка_название*</th><th>*Колонка_название*</th><th>*Колонка_название*</th><th>*Колонка_название*</th>
+            </tr>
+            </thead>
+
+            <tbody>
+
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>
+                <ul>
+                  <li>
+
+                  </li>
+                </ul>
+              </td>
+              <td>
+                <button class="btn btn-xs btn-ghost self-center bg-red-500/80 border-none hover:shadow-none hover:bg-red-900 m-1 active:bg-red-900">Удалить 🗑️</button>
+              </td>
+            </tr>
+
+            </tbody>
+
+          </table>
+        </ScrollArea>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+<script setup lang="ts">
+
+import { useSiteStore } from '~/stores/site_store'
+import {useRoute} from "vue-router"
+
+const site_store = useSiteStore()
+
+const router = useRoute()
+
+
+</script>
